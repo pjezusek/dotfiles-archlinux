78a9e7fb92f498218a674fd14cabf1e96fc656f7
Piotr Jezusek <piotrjez37@gmail.com>
2021-01-04 06:55:57 +0100

    Update

--- fish/conf.d/abbrevations.fish
+++ fish/conf.d/abbrevations.fish
@@ -8,3 +8,4 @@ abbr -a ps ps -aux
 abbr -a th teamocil --here
 abbr -a grep grep --color=auto
 abbr -a diff diff --color=auto
+abbr -a lg lazygit
--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,3 @@
 rvm default
+status --is-interactive; and pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
