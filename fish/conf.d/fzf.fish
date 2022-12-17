8095968208dbf98ed0841e57b479553cff8d486c
Piotr Jezusek <piotr.jezusek@softbeam.pl>
2022-12-17 17:49:55 +0100

    Update

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -2,5 +2,4 @@ set -g FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --color=fg:#e6e6e6,bg:-1,hl:#d92323 -
 set -g FZF_DEFAULT_COMMAND 'ag $dir --hidden --ignore .git -g ""'
 set -g FZF_CTRL_T_OPTS "--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
 set -g FZF_ALT_C_OPTS "--preview 'tree -C {} | head -200'"
-set -g TERM "xterm-256color"
 set -U FZF_LEGACY_KEYBINDINGS 0
